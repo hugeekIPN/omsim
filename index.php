66c8152b573a6218f03fa88ec2f000be5a0892d8
hugeekIPN <vmoralesm1300@alumno.ipn.mx>
2017-11-09 11:25:29 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    
+ <link rel="stylesheet" href="estilos.css">
+    
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    
+<div class="contenedor">
+    
+    <div class="izq">
+        <div class="logo">
+        <img src="imgs/01/logo.gif"> 
+        <div class="titulo">
+           OMSIM ONURES SEROMA
+            <span class="public">PUBLIC PROJECT</span>
+        </div>
+        </div>
+    </div>
+    <div class="der">
+        CONTACTO
+        <form method="post" enctype="application/x-www-form-urlencoded" action="contacto.php" >
+            Mail: <input name="email" type="email" ><br/>
+            Mensaje: <textarea name="message" ></textarea><br/>
+            
+            <input type="submit" value="Enviar" >
+        </form>
+        OMSIM ONURES SEROMA
+        Blbalbala
+        
+        <BR/>
+        <BR/>
+        Síguenos en:<BR/>
+        <a href="https://www.facebook.com/oosppmx/" target="_blank"><img src="iconos/fb/fb.png" onmouseover="$(this).attr('src','iconos/fb/fb_activo.png')"  onmouseout="$(this).attr('src','iconos/fb/fb.png')" class="fb"></a> 
+        
+        <img src="iconos/vimeo/vimeo.png" class="vimeo" onmouseover="$(this).attr('src','iconos/vimeo/vimeo_activo.png')"  onmouseout="$(this).attr('src','iconos/vimeo/vimeo.png')">
+        
+        
+        <img src="iconos/youtube/youtube.png" class="youtube" onmouseover="$(this).attr('src','iconos/youtube/youtube_activo.png')"  onmouseout="$(this).attr('src','iconos/youtube/youtube.png')">
+        <img src="iconos/Instagram/Instagram.png" class="insta" onmouseover="$(this).attr('src','iconos/Instagram/Instagram_activo.png')"  onmouseout="$(this).attr('src','iconos/Instagram/Instagram.png')">
+        <img src="iconos/twitter/twitter.png" class="tw" onmouseover="$(this).attr('src','iconos/twitter/twitter_activo.png')"  onmouseout="$(this).attr('src','iconos/twitter/twitter.png')">
+        
+    </div>
+    
+    </div>    
+    
+</body>
+</html>
